0d83b33d969dbc40ac51d415f5dc9c6516670b9a
Brandon <nikobranart@gmail.com>
2021-03-26 01:16:59 +0200

    Alerts work

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,13 +3,13 @@ import React, { useState } from "react";
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "873-492847" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
-
- 
-
-  console.log('Find' , );
+  const [search, setNewSearch] = useState("");
 
   const addNumber = (event) => {
     event.preventDefault();
@@ -20,16 +20,23 @@ const App = () => {
       name: newName,
       number: newNumber,
     };
-   
-    
-    
-    setPersons(persons.concat(phoneObject));
+
+    const inList = persons.includes(
+      persons.find(({ name }) => name === phoneObject.name)
+    )
+      ? (window.alert(`${newName} is already in use!`),
+        setNewName(""),
+        setNewNumber(""))
+      : setPersons(persons.concat(phoneObject));
+
+    console.log(inList);
+
+    // I guess it is bad to use console.log to use function => warning otherwise
+
+
     setNewName("");
     setNewNumber("");
   };
-
-  
-  
 
   const handleNameChange = (event) => {
     console.log(event.target.value);
@@ -41,6 +48,11 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleSearchChange = (event) => {
+    console.log(event.target.value);
+    setNewSearch(event.target.value);
+  };
+
   const Number = ({ person }) => {
     console.log("Name:", person);
     return (
@@ -50,12 +62,13 @@ const App = () => {
     );
   };
 
-
-  
-
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={search} onChange={handleSearchChange}></input>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addNumber}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
