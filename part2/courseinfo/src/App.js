de13baa3a5535ae34c3d0ada07e6b5c27822b2a9
Brandon <nikobranart@gmail.com>
2021-03-25 16:06:56 +0200

    Back upbefore making modules

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,32 +1,64 @@
 import React from "react";
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    id: 1,
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
+  
+
   return (
     <div>
-      <Course course={course} />
+      <h1>Web development curriculum</h1>
+      {courses.map(i => <Course course={i}/>)}
     </div>
+    /*
+    <div>
+      <Course course={courses[0]} />
+      <Course course={courses[1]} />
+    </div>
+    */
   );
 };
 
@@ -43,7 +75,7 @@ const Course = (props) => {
 
 const Header = (props) => {
   console.log("Header props: ", props);
-  return <h1>{props.name}</h1>;
+  return <h2>{props.name}</h2>;
 };
 
 const Content = ({ parts }) => {
@@ -65,9 +97,7 @@ const Part = ({ part }) => {
     return s + p.exercises;
   }, 0);
 
-  console.log('sum from reduce', total);
-
-
+  console.log("sum from reduce", total);
 
   return (
     <div>
@@ -76,9 +106,7 @@ const Part = ({ part }) => {
           {p.name} {p.exercises}
         </p>
       ))}
-      <p>
-        total of {total} exercises
-      </p>
+      <p style={{fontWeight: "bold"}}>total of {total} exercises</p>
     </div>
   );
 };
