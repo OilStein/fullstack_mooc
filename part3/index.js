5dc1bb351f9ce04d1ecb575ac88007690bc86d54
Brandon <nikobranart@gmail.com>
2021-04-29 14:11:02 +0300

    Trying to get fronend to work

--- part3/index.js
+++ part3/index.js
@@ -3,9 +3,6 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors")
 const Person = require('./models/person');
-const {
-  request
-} = require('express');
 const app = express();
 
 app.use(express.json());
