d911f8285f818d74a2119d719ca8ecd5b0244d4c
Brandon <nikobranart@gmail.com>
2021-05-04 23:14:49 +0300

    Part 3 finished. Eslint standard

--- part3/index.js
+++ part3/index.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors")
-const Person = require('./models/person');
-const app = express();
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const Person = require('./models/person')
 
-app.use(express.json());
+const app = express()
+
+app.use(express.json())
 
 app.use(cors())
 
@@ -14,43 +15,39 @@ app.use(express.static('build'))
 //   return res.statusCode
 // }))
 
-morgan.token("post", (req, res) => {
-  return JSON.stringify(req.body);
-});
+morgan.token('post', (req, _res) => JSON.stringify(req.body))
 
 app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :post")
-);
+  morgan(':method :url :status :res[content-length] - :response-time ms :post')
+)
 
-
-app.get("/info", (req, res, next) => {
-  Person.countDocuments({}).then(c => {
-    const date = new Date();
+app.get('/info', (_req, res, _next) => {
+  Person.countDocuments({}).then((c) => {
+    const date = new Date()
     res.send(
-      `<div> Phonebook has info for ${c} people </div>` + `<div>${date}</div>`
-    );
+      `<div> Phonebook has info for ${c} people </div>`, `<div>${date}</div>`
+    )
   })
+})
 
-});
-
-app.get("/api/persons", (req, res) => {
-  Person.find({}).then(persons => {
+app.get('/api/persons', (_req, res) => {
+  Person.find({}).then((persons) => {
     res.json(persons)
   })
-});
+})
 
-app.get("/api/persons/:id", (req, res, next) => {
-  Person.findById(req.params.id).then(person => {
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id).then((person) => {
     res.json(person)
-  }).catch(error => next(error))
-});
+  }).catch((error) => next(error))
+})
 
-app.delete("/api/persons/:id", (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
-    .then(_result => {
+    .then(() => {
       res.status(204).end()
-    }).catch(e => next(e))
-});
+    }).catch((e) => next(e))
+})
 
 // const getRandomId = (min, max) => {
 //   min = Math.ceil(min);
@@ -60,15 +57,15 @@ app.delete("/api/persons/:id", (req, res) => {
 
 // console.log(getRandomId(1, 100000));
 
-app.post("/api/persons", (req, res, next) => {
-  const body = req.body;
+app.post('/api/persons', (req, res, next) => {
+  const { body } = req
 
   // console.log('body content',body);
 
   if (body.name === undefined && body.number === undefined) {
     return res.status(400).json({
-      error: "Name or number missing",
-    });
+      error: 'Name or number missing'
+    })
   }
 
   const person = new Person({
@@ -76,31 +73,31 @@ app.post("/api/persons", (req, res, next) => {
     number: body.number
   })
 
-  person.save().then(savedPerson => {
+  person.save().then((savedPerson) => {
     res.json(savedPerson.toJSON())
-  }).catch(e => next(e))
-});
+  }).catch((e) => next(e))
+})
 
 app.put('/api/persons/:id', (req, res, next) => {
-  const body = req.body
+  const { body } = req
   const person = {
     name: body.name,
-    number: body.number,
+    number: body.number
   }
 
   Person.findByIdAndUpdate(req.params.id, person, {
     new: true
-  }).then(updatePerson => {
+  }).then((updatePerson) => {
     res.json(updatePerson)
-  }).catch(e => next(e))
+  }).catch((e) => next(e))
 })
 
-const PORT = process.env.PORT;
+const { PORT } = process.env
 app.listen(PORT, () => {
-  console.log(`Server on ${PORT}`);
-});
+  console.log(`Server on ${PORT}`)
+})
 
-const unknownEndpoint = (req, res) => {
+const unknownEndpoint = (_req, res) => {
   res.status(404).send({
     error: 'unknown endpoint'
   })
@@ -108,21 +105,21 @@ const unknownEndpoint = (req, res) => {
 
 app.use(unknownEndpoint)
 
-const errorHandler = (error, req, res, next) => {
-  console.error(error.message);
+const errorHandler = (error, _req, res, next) => {
+  console.error(error.message)
   if (error.name === 'castError') {
     return res.status(400).send({
       error: 'malformated id'
     })
-  } else if (error.name === 'ValidationError') {
+  } if (error.name === 'ValidationError') {
     return res.status(400).json({
       error: error.message
     })
-  } else if (error.name === 'ValidatorError') {
+  } if (error.name === 'ValidatorError') {
     return res.status(409).json({
       error: error.message
     })
-  } 
+  }
   next(error)
 }
 
